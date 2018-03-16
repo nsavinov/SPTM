e427c8c1b201e86f1e166efe6c2c070600baa572
Nikolay Savinov <nsavinov@cvg-desktop-15.inf.ethz.ch>
2018-03-16 17:11:24 +0100

    fix

--- src/common/__init__.py
+++ src/common/__init__.py
@@ -1,10 +1,3 @@
 from constants import *
 from resnet import *
 from util import *
-
-# limit memory usage
-import tensorflow as tf
-from keras.backend.tensorflow_backend import set_session
-config = tf.ConfigProto()
-config.gpu_options.per_process_gpu_memory_fraction = TEST_MEMORY_FRACTION
-set_session(tf.Session(config=config))
--- src/test/test_setup.py
+++ src/test/test_setup.py
@@ -13,3 +13,10 @@ def test_setup(wad):
   game = doom_navigation_setup(TEST_RANDOM_SEED, wad)
   wait_idle(game, WAIT_BEFORE_START_TICS)
   return game
+
+# limit memory usage
+import tensorflow as tf
+from keras.backend.tensorflow_backend import set_session
+config = tf.ConfigProto()
+config.gpu_options.per_process_gpu_memory_fraction = TEST_MEMORY_FRACTION
+set_session(tf.Session(config=config))
